09db8973ddb8fbba58481d1fb9bbd8ccb8465120
camila1081 <camila1081@gmail.com>
2022-04-16 02:05:49 -0300

    updating code 3

--- etapa_twitter.py
+++ etapa_twitter.py
@@ -11,7 +11,7 @@ def run_twitter():
     bearer_token = os.environ.get("BEARER_TOKEN")
     project_name='boti-347200'
     #search_url = "https://api.twitter.com/2/tweets/search/recent"
-    query = 'Boticário maquiagem'
+    query = 'Boticário OR Maquiagem'
     tweet_fields = "tweet.fields=author_id,conversation_id,created_at,id,in_reply_to_user_id,public_metrics,text"
     user_fields = "expansions=author_id&user.fields=id,name,username,created_at"
     max_results ="max_results=50"
@@ -23,7 +23,7 @@ def run_twitter():
     # Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
     # expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
     #query_params = {'query': '(from:twitterdev -is:retweet) OR #twitterdev','tweet.fields': 'author_id',"max_results" : 100}
-    query_params = {"max_results" : 100}
+    query_params = {"max_results":100}
 
     #json_response = connect_to_endpoint(self.search_url, self.query_params)
     def bearer_oauth(r):
@@ -49,14 +49,17 @@ def run_twitter():
 
     df_text=pd.DataFrame(data[:])
     df_text=pd.DataFrame(df_text,columns=['text'])
-    df_name=pd.DataFrame(users['users'],columns=['name'])
-
+    df_name=pd.DataFrame(users['users'])
+    df_name=pd.DataFrame(df_name['username'],columns=['username'])
+    df_name.reset_index()
+    df_text.reset_index()
     
     df_bq=pd.concat([df_name,df_text],axis=1)
     df_bq.reset_index()
+    df_bq=df_bq.dropna()
     print(df_bq)
 
-    df_bq.to_gbq(destination_table = 'dados_twitter.twits_recentes', project_id=project_name,if_exists='replace' )
+    df_bq[0:49].to_gbq(destination_table = 'dados_twitter.twits_recentes', project_id=project_name,if_exists='replace' )
 
 
 
