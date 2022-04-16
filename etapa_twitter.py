64bcf09af5b9de0f2f279db3a5be1d7c166e4c0b
camila1081 <camila1081@gmail.com>
2022-04-16 16:23:44 -0300

    updating code 4

--- etapa_twitter.py
+++ etapa_twitter.py
@@ -1,31 +1,27 @@
 import requests
 import os
-import json
 import pandas as pd
-from google.cloud import bigquery
 
 
 # To set your environment variables in your terminal run the following line:
 # export 'BEARER_TOKEN'='<your_bearer_token>'
 def run_twitter():
     bearer_token = os.environ.get("BEARER_TOKEN")
-    project_name='boti-347200'
-    #search_url = "https://api.twitter.com/2/tweets/search/recent"
-    query = 'Boticário OR Maquiagem from:twitterdev -is:retweet'
-    tweet_fields = "tweet.fields=author_id,conversation_id,created_at,id,in_reply_to_user_id,public_metrics,text"
+    project_name = "boti-347200"
+    # search_url = "https://api.twitter.com/2/tweets/search/recent"
+    query = "Boticário OR Maquiagem from:twitterdev -is:retweet lang:pt"
+    tweet_fields = "tweet.fields=author_id,conversation_id,created_at,id,in_reply_to_user_id,public_metrics,text,lang"
     user_fields = "expansions=author_id&user.fields=id,name,username,created_at"
-    max_results ="max_results=50"
-    lingua = " lang%3Apt"
-    #search_url= "https://api.twitter.com/2/tweets/search/recent?query=Botic%C3%A1rio&src=typed_query"
-    search_url = "https://api.twitter.com/2/tweets/search/recent?query={}{}&{}&{}".format(
-            query, lingua,tweet_fields,user_fields
-        )
+    # lingua = "lang%3Apt"
+    search_url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}&{}".format(
+        query, tweet_fields, user_fields
+    )
     # Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
     # expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
-    #query_params = {'query': '(from:twitterdev -is:retweet) OR #twitterdev','tweet.fields': 'author_id',"max_results" : 100}
-    query_params = {"max_results":100}
+    # query_params = {'query': '(from:twitterdev -is:retweet) OR #twitterdev','tweet.fields': 'author_id',"max_results" : 100}
+    query_params = {"max_results": 100}
 
-    #json_response = connect_to_endpoint(self.search_url, self.query_params)
+    # json_response = connect_to_endpoint(self.search_url, self.query_params)
     def bearer_oauth(r):
         """
         Method required by bearer token authentication.
@@ -35,35 +31,32 @@ def run_twitter():
         r.headers["User-Agent"] = "v2RecentSearchPython"
         return r
 
-    response = requests.get(search_url, auth=bearer_oauth,params=query_params)
-    
+    response = requests.get(search_url, auth=bearer_oauth, params=query_params)
+
     if response.status_code != 200:
         raise Exception(response.status_code, response.text)
-    
+
     json_response = response.json()
 
-    
-    data=json_response.get("data")
-    meta=json_response.get("meta")
-    users=json_response.get("includes")
+    data = json_response.get("data")
+    users = json_response.get("includes")
 
-    df_text=pd.DataFrame(data[:])
-    df_text=pd.DataFrame(df_text,columns=['text'])
-    df_name=pd.DataFrame(users['users'])
-    df_name=pd.DataFrame(df_name['username'],columns=['username'])
-    df_name.reset_index()
-    df_text.reset_index()
-    
-    df_bq=pd.concat([df_name,df_text],axis=1)
-    df_bq.reset_index()
-    df_bq=df_bq.dropna()
-    print(df_bq)
+    df_text = pd.DataFrame(data[:])
+    df_text = pd.DataFrame(df_text, columns=["text", "author_id", "lang"])
+    df_text = df_text[df_text.lang == "pt"]
+    df_name = pd.DataFrame(users["users"])
+    df_name = df_name[["username", "id"]]
 
-    df_bq[0:49].to_gbq(destination_table = 'dados_twitter.twits_recentes', project_id=project_name,if_exists='replace' )
+    df_bq = df_name.merge(df_text, left_on="id", right_on="author_id")
+    df_bq = df_bq.dropna()
+    df_bq = df_bq[["username", "text"]]
+    print(df_bq[0:49])
 
-
-
-
+    df_bq[0:49].to_gbq(
+        destination_table="dados_twitter.tweets_recentes",
+        project_id=project_name,
+        if_exists="replace",
+    )
 
 
 if __name__ == "__main__":
